bd51ef7d4a30fdc5c0c51a46a0c741ed4e0d678b
Roberto Villalva <rvillalva@RV-0401.local>
2016-01-20 11:54:51 -0800

    Deleted migration files for adding/removing x_firts_name to Products

--- db/migrate/20160119181829_add_x_first_name_to_products.rb
+++
@@ -1,5 +0,0 @@
-class AddXFirstNameToProducts < ActiveRecord::Migration
-  def change
-    add_column :products, :x_first_name, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160119205956) do
+ActiveRecord::Schema.define(version: 20160120193930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,9 +23,8 @@ ActiveRecord::Schema.define(version: 20160119205956) do
     t.integer  "price"
     t.string   "category"
     t.string   "subcategory"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.string   "x_first_name"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
