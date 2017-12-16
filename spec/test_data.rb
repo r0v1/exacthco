0684c3f01eb5b454cb92726c90028da9fb53510c
Roberto Villalva <rvillalva@e-xact.com>
2017-12-15 17:08:37 -0800

    Added testing for a data set.

---
+++ spec/test_data.rb
@@ -0,0 +1,57 @@
+module TestData
+
+  def initialize_data
+
+    [
+      {pp_amount: "10.00",
+       pp_name:   "VISA",
+       pp_ccnum:  "4111111111111111",
+       pp_date:   "1222",
+       pp_email:  "email1@example.com"},
+       
+      {pp_amount: "20.00",
+       pp_name:   "MC5",
+       pp_ccnum:  "5500000000000004",
+       pp_date:   "1222",
+       pp_email:  "email2@example.com"},
+
+      {pp_amount: "30.00",
+       pp_name:   "MC2",
+       pp_ccnum:  "2223000148400010",
+       pp_date:   "1222",
+       pp_email:  "email3@example.com"},
+
+      {pp_amount: "40.00",
+       pp_name:   "Amex",
+       pp_ccnum:  "340000000000009",
+       pp_date:   "1222",
+       pp_email:  "email4@example.com"},
+
+      {pp_amount: "50.00",
+       pp_name:   "JCB",
+       pp_ccnum:  "3566002020140006",
+       pp_date:   "1222",
+       pp_email:  "email5@example.com"},
+
+      {pp_amount: "60.00",
+       pp_name:   "Discover",
+       pp_ccnum:  "6011000000000004",
+       pp_date:   "1222",
+       pp_email:  "email6@example.com"},
+
+      {pp_amount: "70.00",
+       pp_name:   "DC",
+       pp_ccnum:  "36438999960016",
+       pp_date:   "1222",
+       pp_email:  "email7@example.com"},
+
+      {pp_amount: "80.00",
+       pp_name:   "Discover CUP",
+       pp_ccnum:  "6250941006528599",
+       pp_date:   "1222",
+       pp_email:  "email8@example.com"}
+     ]
+
+  end
+
+end
