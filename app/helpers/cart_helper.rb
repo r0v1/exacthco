a4a053f83c0ffa99218407cae8b991c73d9ea940
Roberto Villalva <rvillalva@e-xact.com>
2019-03-07 12:47:55 -0800

    Added a few PPs for ETQA; refactored some E4 PPs; small changes on cart index view.

--- app/helpers/cart_helper.rb
+++ app/helpers/cart_helper.rb
@@ -1,2 +1,25 @@
 module CartHelper
+
+  def generate_sequence_number
+    ((rand*100000).to_i + 5000).to_s
+  end
+
+  def generate_random_amount_15_30
+    (rand(15..30).to_i).to_s
+  end
+
+  def generate_timestamp
+    Time.now.to_i.to_s
+  end
+
+  def generate_hmac_string(login, sequence, timestamp, amount, currency)
+     # Concatenating the values included in the x_fp_hash
+     # hmac_string = "#{x_login}^#{x_fp_sequence}^#{x_fp_timestamp}^#{x_amount}^#{x_currency_code}"
+     "#{login}^#{sequence}^#{timestamp}^#{amount}^#{currency}"
+  end
+
+  def generate_md5_hash(transaction_key, hmac_string)
+     OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("md5"), transaction_key, hmac_string)
+  end
+
 end
